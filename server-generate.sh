5a184e102ee6c561a4c25041a1b9b74c0e0fec38
Ahmet OZER <git@ahmetozer.org>
2020-11-20 23:20:22 +0300

    Update IPv6 regex to match range set default ipv6 range control ip_block,netmask and ip6_block fast_install variable fix #105 one_config file added

--- server-generate.sh
+++ server-generate.sh
@@ -6,14 +6,40 @@
 
 port_regex="^((6553[0-5])|(655[0-2][0-9])|(65[0-4][0-9]{2})|(6[0-4][0-9]{3})|([1-5][0-9]{4})|([1-9][0-9]{3})|([1-9][0-9]{2})|([1-9][0-9])|([1-9]))$"
 ip_regex="^(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4})"
-ip6_regex="^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$"
+ip6_regex="^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))(/[1-9][0-9])?$"
+server_config_dir=${server_config_dir-/etc/openvpn/server}
 
-mkdir -p /etc/openvpn/server/
-if [ -f /etc/openvpn/server/env ]; then
+ip_block=${ip_block-10.0.1.0}
+netmask=${netmask-255.255.255.0}
+
+ip6_block=${ip6_block-"fdac:900d:c0ff:ee::/64"}
+
+
+echo "Config dir $server_config_dir"
+mkdir -pv $server_config_dir/oneconfig
+if [ -f $server_config_dir/env ]; then
     echo "Server is already configured and installed"
     exit 0
-else
-    touch /etc/openvpn/server/env
+fi
+
+DATE=`date`
+
+if [[ ! "$ip_block" =~ $ip_regex ]]
+then
+    echo "Your ip block is not right $ip_block"
+    exit 1
+fi
+
+if [[ ! "$netmask" =~ $ip_regex ]]
+then
+    echo "Your netmask is not right $netmask"
+    exit 1
+fi
+
+if [[ ! "$ip6_block" =~ $ip6_regex ]]
+then
+    echo "Your IPv6 block or range is not right $ip6_block"
+    exit 1
 fi
 
 until [[ "$protocol" =~ "^tcp$|^udp$" ]]; do
@@ -36,6 +62,7 @@ until [[ "$protocol" =~ "^tcp$|^udp$" ]]; do
     esac
 done
 echo "Protocol $protocol"
+
 
 until [[ "$port" =~ $port_regex ]]; do
 
@@ -75,7 +102,7 @@ echo "Port $port"
 
 until [[ "$dev_type" =~ "^tun$|^tap$" ]]; do
     if [ "$fast_install" == 'true' ] && [ -z "$dev_type" ]; then
-        protocol='tun'
+        dev_type='tun'
     else
         read -p "What device type is do you want to use for VPN ? (tun/tap) > " -e -i "tun" dev_type
     fi
@@ -113,9 +140,11 @@ until [[ "$dns2" =~ $ip_regex ]] || [[ "$dns2" =~ $ip6_regex ]]; do
 done
 echo "DNS 2 $dns2"
 
-# Write dh.pem
+touch $server_config_dir/env
+echo "DATE=$DATE" > $server_config_dir/env
 
-# https://ssl-config.mozilla.org/ffdhe2048.txt
+# Write dh.pem
+# ? https://ssl-config.mozilla.org/ffdhe2048.txt
 echo "-----BEGIN DH PARAMETERS-----
 MIIBCAKCAQEA//////////+t+FRYortKmq/cViAnPTzx2LnFg84tNpWp4TZBFGQz
 +8yTnc4kmz75fS/jY2MMddj2gbICrsRhetPfHtXV/WVhJDP1H18GbtCFY2VVPe0a
@@ -123,12 +152,12 @@ MIIBCAKCAQEA//////////+t+FRYortKmq/cViAnPTzx2LnFg84tNpWp4TZBFGQz
 YdEIqUuyyOP7uWrat2DX9GgdT0Kj3jlN9K5W7edjcrsZCwenyO4KbXCeAvzhzffi
 7MA0BM0oNC9hkXL+nOmFg/+OTxIy7vKBg8P+OxtMb61zO7X8vC7CIAXFjvGDfRaD
 ssbzSibBsu/6iGtCOGEoXJf//////////wIBAg==
------END DH PARAMETERS-----"  >/etc/openvpn/server/dh.pem
+-----END DH PARAMETERS-----" >$server_config_dir/dh.pem
 
 current_dir=$PWD
 echo "Creating Server Certifaces"
 # ? trap 'capture err' ERR
-cd /etc/openvpn/server/easy-rsa/
+cd $server_config_dir/easy-rsa/
 
 echo "EASYRSA_CERT_EXPIRE ${EASYRSA_CERT_EXPIRE-3650}"
 echo "EASYRSA_CRL_DAYS ${EASYRSA_CRL_DAYS-3650}"
@@ -137,33 +166,30 @@ echo "EASYRSA_CRL_DAYS ${EASYRSA_CRL_DAYS-3650}"
 ./easyrsa --batch build-ca nopass
 ./easyrsa build-server-full server nopass
 ./easyrsa gen-crl
-cp pki/ca.crt pki/private/ca.key pki/issued/server.crt pki/private/server.key pki/crl.pem /etc/openvpn/server
-chmod o+x /etc/openvpn/server/
-openvpn --genkey --secret /etc/openvpn/server/tc.key
+cp pki/ca.crt pki/private/ca.key pki/issued/server.crt pki/private/server.key pki/crl.pem $server_config_dir
+chmod o+x $server_config_dir/
+openvpn --genkey --secret $server_config_dir/tc.key
 # ? trap - ERR #! Reset the trap
-ls -lah /etc/openvpn/server/
+ls -lah $server_config_dir/
 
-echo "protocol=$protocol" >/etc/openvpn/server/env
-echo "port=$port" >>/etc/openvpn/server/env
-echo "dns1=$dns1" >>/etc/openvpn/server/env
-echo "dns2=$dns2" >>/etc/openvpn/server/env
+echo "protocol=$protocol" >> $server_config_dir/env
+echo "port=$port" >>$server_config_dir/env
+echo "dns1=$dns1" >>$server_config_dir/env
+echo "dns2=$dns2" >>$server_config_dir/env
 
+echo "EASYRSA_CERT_EXPIRE=${EASYRSA_CERT_EXPIRE-3650}" >>$server_config_dir/env
+echo "EASYRSA_CRL_DAYS=${EASYRSA_CRL_DAYS-3650}" >>$server_config_dir/env
 
-echo "EASYRSA_CERT_EXPIRE=${EASYRSA_CERT_EXPIRE-3650}" >>/etc/openvpn/server/env
-echo "EASYRSA_CRL_DAYS=${EASYRSA_CRL_DAYS-3650}" >>/etc/openvpn/server/env
+echo "###   ###
+# Hostname $HOSTNAME
+# Date $DATE
+# Source https://github.com/ahmetozer/openvpn-config-generator-container
 
-echo "port $port
+port $port
 proto $protocol
 #dev vpn1
 #dev-type $dev_type
 dev $dev_type
-ca ca.crt
-cert server.crt
-key server.key
-dh dh.pem
-crl-verify crl.pem
-auth SHA512
-tls-crypt tc.key
 
 topology subnet
 
@@ -176,15 +202,58 @@ status ${openvpn_status_log_loc-/dev/null} #? Log location. Default openvpn-stat
 verb ${verb-0}
 
 
-server 10.8.0.0 255.255.255.0
 keepalive 10 120
+server 10.8.0.0 255.255.255.0
+#push \"redirect-gateway def1 bypass-dhcp\"
+push \"redirect-gateway def1 ipv6 bypass-dhcp\"
 
-push \"redirect-gateway def1 bypass-dhcp\"
 ifconfig-pool-persist ipp.txt
 push \"dhcp-option DNS $dns1\"
 push \"dhcp-option DNS $dns2\"
-" >>/etc/openvpn/server/server.conf
-
+" >>$server_config_dir/server.conf
 if [[ "$protocol" = "udp" ]]; then
-    echo "explicit-exit-notify" >>/etc/openvpn/server/server.conf
+    echo "sndbuf 300000
+rcvbuf 300000
+#fast-io
+explicit-exit-notify" >>$server_config_dir/server.conf
 fi
+
+###
+#  ! Set certiface configure to config
+###
+cp $server_config_dir/server.conf $server_config_dir/oneconfig/server.conf
+echo "
+ca ca.crt
+cert server.crt
+key server.key
+dh dh.pem
+crl-verify crl.pem
+auth SHA512
+tls-crypt tc.key
+" >>$server_config_dir/server.conf
+
+# * Embed certifaces to server.conf to more easy transfer configuration
+
+echo "<ca>" >>$server_config_dir/oneconfig/server.conf
+cat $server_config_dir/ca.crt >>$server_config_dir/oneconfig/server.conf
+echo "</ca>" >>$server_config_dir/oneconfig/server.conf
+
+echo "<cert>" >>$server_config_dir/oneconfig/server.conf
+cat $server_config_dir/server.crt >>$server_config_dir/oneconfig/server.conf
+echo "</cert>" >>$server_config_dir/oneconfig/server.conf
+
+echo "<key>" >>$server_config_dir/oneconfig/server.conf
+cat $server_config_dir/server.key >>$server_config_dir/oneconfig/server.conf
+echo "</key>" >>$server_config_dir/oneconfig/server.conf
+
+echo "<dh>" >>$server_config_dir/oneconfig/server.conf
+cat $server_config_dir/dh.pem >>$server_config_dir/oneconfig/server.conf
+echo "</dh>" >>$server_config_dir/oneconfig/server.conf
+
+echo "<crl-verify>" >>$server_config_dir/oneconfig/server.conf
+cat $server_config_dir/crl.pem >>$server_config_dir/oneconfig/server.conf
+echo "</crl-verify>" >>$server_config_dir/oneconfig/server.conf
+
+echo "<tls-crypt>" >>$server_config_dir/oneconfig/server.conf
+cat $server_config_dir/tc.key >>$server_config_dir/oneconfig/server.conf
+echo "</tls-crypt>" >>$server_config_dir/oneconfig/server.conf
